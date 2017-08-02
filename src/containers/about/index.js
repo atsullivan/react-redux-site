d9d01a4f0097e282b94612000ad5f8028656ff22
Andrew Sullivan <andrew.sullivan@procore.com>
2017-08-02 11:01:44 -0700

    Start basic 'About Me' section

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import '../../styles/about.css'
 
 const About = () => (
   <div>
-    <h1>About Page</h1>
+    <div className="header">
+      <h1>About</h1>
+      <p>
+        I am a software engineer living in Santa Barbara California.
+        I specialize in Rails and React.
+      </p>
+    </div>
+
+    <div className="my-story">
+      <h2>My Story</h2>
+      <hr/>
+      <p>
+        My name is Andrew Sullivan, but everyone calls me Sully.
+        I graduated from Loyola Marymount University in 2015 with a Bachelor's Degree in Computer Science.
+        I currenly work at Procore Technologies as a Rails Engineer.
+        In my free time I like to design User Interfaces and experiment with new technologies.
+      </p>
+    </div>
+
+    <div>
+      <h2>What I Do</h2>
+      <hr/>
+    </div>
+
+    <div>
+      <h3>Web Development</h3>
+    </div>
+
+    <div>
+      <h3>UI Design</h3>
+    </div>
+
+    <div>
+      <h3>Web Security</h3>
+    </div>
+
+    <div>
+      <h3>Brand Development</h3>
+    </div>
   </div>
 )
 
